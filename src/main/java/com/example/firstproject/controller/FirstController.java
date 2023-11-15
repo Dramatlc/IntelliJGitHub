f0c1e3080b96f34cd90fcd01a82068ecebeb7d65
Dramatic <y97791013@gmail.com>
2023-11-15 16:04:17 +0900

    first commit

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","정식");
+        return "greetings";
+    }
+}
